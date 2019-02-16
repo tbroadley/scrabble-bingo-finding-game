c3ac4ccde1485f57f6b8f901c76c1213eeae63f6
Thomas Broadley <buriedunderbooks@hotmail.com>
2019-02-16 06:03:22 -0500

    Make a simple Yesod application

--- Main.hs
+++ Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+
 module Main where
 
+import Yesod
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = warp 3000 App
