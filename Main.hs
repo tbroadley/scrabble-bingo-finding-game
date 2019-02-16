f884d9d3f9e85fd95ea6428e69da893382b39440
Thomas Broadley <buriedunderbooks@hotmail.com>
2019-02-16 14:37:18 -0500

    Add option to limit words by frequency

--- Main.hs
+++ Main.hs
@@ -7,13 +7,16 @@
 module Main where
 
 import Control.Monad.Loops
+import Data.Char
 import Data.List
 import Data.Text (pack, strip, unpack)
 import System.Random
 import System.Random.Shuffle
+import Text.Read
 import Yesod
 
-data App = App { bingos :: [String]
+data App = App { bingos        :: [String],
+                 frequentWords :: [String]
                }
 
 mkYesod "App" [parseRoutes|
@@ -29,29 +32,44 @@ getHomeR :: Handler Html
 getHomeR = do
   App {..} <- getYesod
 
-  randomIndex <- liftIO $ getStdRandom (randomR (1, length bingos))
-  let word = bingos !! randomIndex
-  shuffledWord <- liftIO $ shuffleUntilNotInDictionary bingos word
+  common <- lookupGetParam "common"
+  let commonBingos = case common of
+                       Just c  -> case readMaybe $ unpack c of
+                                    Just i  -> intersect bingos $ take i frequentWords
+                                    Nothing -> bingos
+                       Nothing -> bingos
 
-  let sortedWord = sort word
-  let matchingBingos = filter ((== sortedWord) . sort) bingos
+  if null commonBingos then invalidArgs [pack "common"] else do
+    randomIndex <- liftIO $ getStdRandom (randomR (1, length commonBingos - 1))
+    let word = commonBingos !! randomIndex
+    shuffledWord <- liftIO $ shuffleUntilNotInDictionary commonBingos word
 
-  defaultLayout [whamlet|
-    <p>
-      These seven letters: #{shuffledWord}
-    <p>
-      Can be unscrambled into the following bingos:
-      <ul>
-        $forall bingo <- matchingBingos
-          <li>#{bingo}
-  |]
+    let sortedWord = sort word
+    let matchingBingos = filter ((== sortedWord) . sort) bingos
+
+    defaultLayout [whamlet|
+      <p>
+        These seven letters: #{shuffledWord}
+      <p>
+        Can be unscrambled into the following bingos:
+        <ul>
+          $forall bingo <- matchingBingos
+            <li>#{bingo}
+    |]
+
+clean :: String -> String
+clean = unpack . strip . pack
 
 main :: IO ()
 main = do
          dictionary <- readFile "dictionary.txt"
-         let cleanedDictionary = map (unpack . strip . pack) $ lines dictionary
+         let cleanedDictionary = map clean $ lines dictionary
          let bingos = filter ((== 7) . length) cleanedDictionary
 
+         wordFrequencyList <- readFile "word-frequencies.txt"
+         let frequentWords = map (map toUpper . head . words) $ lines wordFrequencyList
+
          warp 3000 App
-           { bingos = bingos
+           { bingos = bingos,
+             frequentWords = frequentWords
            }
