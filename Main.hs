73c4a8646c89391b3c0c77a61554490d97960f24
Thomas Broadley <buriedunderbooks@hotmail.com>
2019-02-16 06:41:17 -0500

    Render a random seven-letter Scrabble word

--- Main.hs
+++ Main.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
 
 module Main where
 
+import Data.Text (pack, strip, unpack)
+import System.Random
 import Yesod
 
-data App = App
+data App = App { bingos :: [String]
+               }
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
@@ -16,7 +20,17 @@ mkYesod "App" [parseRoutes|
 instance Yesod App
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = do
+  App {..} <- getYesod
+  randomIndex <- liftIO $ getStdRandom (randomR (1, length bingos))
+  defaultLayout [whamlet|
+    A random seven-letter Scrabble word: #{bingos !! randomIndex}
+  |]
 
 main :: IO ()
-main = warp 3000 App
+main = do
+         dictionary <- readFile "dictionary.txt"
+         let cleanedDictionary = map (unpack . strip . pack) . lines $ dictionary
+         warp 3000 App
+           { bingos = filter ((== 7) . length) cleanedDictionary
+           }
